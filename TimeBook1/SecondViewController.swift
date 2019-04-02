7e72e1ab069f8089b0d85e6157b83beb2b717ce1
Matthew Hayward <matthewlvh1@me.com>
2019-04-02 05:53:34 -0700

    Initial Commit

---
+++ TimeBook1/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TimeBook1
+//
+//  Created by Matthew Hayward on 4/2/19.
+//  Copyright © 2019 2ndLawApps. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
